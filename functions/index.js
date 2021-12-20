25dda9c37050331cdbe093dc75d3aac1bcb10ab6
Rainier <rainiermichelena@gmail.com>
2021-12-20 12:45:11 -0500

    Add standard path to constant.

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,9 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
 admin.initializeApp(functions.config().firebase);
+const standardPath = "/data/pacientes";
 
-const ref = admin.database().ref("/data/pacientes");
+const ref = admin.database().ref(standardPath);
 const refLogs = admin.database().ref("/data/logs");
 
 // Función para almacenar logs despues de una operación sobre un endpoint.
@@ -16,11 +17,10 @@ const logsFunction = (action) => {
 };
 
 exports.pacientesData = functions.https.onRequest((request, response) => {
-  const refById = admin.database().ref(`/data/pacientes/${request.query.id}`);
-  logsFunction("Acceso a endpoint GET data/pacientes");
-
+  const refById = admin.database().ref(`${standardPath}/${request.query.id}`);
   if (request.method === "GET") {
     if (request.query.id && request.query.id !== "") {
+      logsFunction(`Acceso a endpoint GET ${standardPath}/:id`);
       refById.on("value", (snap) => {
         if (snap.val().accesible) {
           return response.status(200).send(JSON.stringify(snap.val()));
@@ -29,6 +29,7 @@ exports.pacientesData = functions.https.onRequest((request, response) => {
         }
       });
     } else {
+      logsFunction(`Acceso a endpoint GET ${standardPath}`);
       ref.on("value", (snap) =>
         response.status(200).send(JSON.stringify(snap.val()))
       );
@@ -36,6 +37,7 @@ exports.pacientesData = functions.https.onRequest((request, response) => {
   }
 
   if (request.method === "POST" && request.body) {
+    logsFunction(`Acceso a endpoint POST ${standardPath}`);
     const childRef = ref.push();
     childRef.set({
       name: request.body.name,
